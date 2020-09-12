b0573b0aa2152501ee1a4958a10f3d36b4c8dae2
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2020-09-12 12:36:42 -0300

    Solve linked list-based questions

---
+++ app/src/main/java/com/jacksonfdam/crackinginterview/list/LinkedList.kt
@@ -0,0 +1,27 @@
+package com.jacksonfdam.crackinginterview.list
+
+class LinkedList<T> {
+
+    private var numberOfElements = 0
+    private var head: Node? = null
+
+    fun add(element: T) {
+        numberOfElements++
+        TODO()
+    }
+
+    fun add(element: T, position: Int): Unit = TODO()
+
+    fun remove(position: Int): Unit = TODO()
+
+    fun get(position: Int): T = TODO()
+
+    fun size(): Int = numberOfElements
+
+    fun isEmpty() = numberOfElements == 0
+
+    private inner class Node(val data: T) {
+        fun next(): Node? = null
+    }
+}
+
